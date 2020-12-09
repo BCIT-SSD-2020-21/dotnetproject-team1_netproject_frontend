24e3fd2b4fc0672a0b7e096cee4b9c1fb4a4c4b1
Patrick Fortaleza <patrickfortaleza@gmail.com>
2020-12-08 19:52:47 -0800

    Chat screen ui config

--- src/components/Chat/ChatMessage.js
+++ src/components/Chat/ChatMessage.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
+import { UpTriangle, DownTriangle } from '../Icons'
 
 export class ChatMessage extends Component {
   render() {
     return (
-      <li> 
-        <img src="https://pfteza-chatapp.s3-us-west-1.amazonaws.com/iconmonstr-user-20.svg" alt="profile-image" />
-        <div className="messageBody">
-            
-            <span className="username">Frodo Baggins</span> <br/>
-            <span className="message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et esse iusto maiores sit omnis itaque minus sint. Ullam, quis.</span>
-        </div>
-
-        <div className="messageMeta">
-            September 20, 2021&nbsp;•&nbsp;
-            <button
-              style={{color: "#E52646"}}
-            >
-                Delete
-            </button>
+      <li>
+        <div className="chat__container"> 
+          <div className="chat__col left">
+              <div className="img-container">
+                <img src="https://pfteza-chatapp.s3-us-west-1.amazonaws.com/iconmonstr-user-20.svg" alt="profile-image" />
+              </div>
+              <div className="rating">
+                  <span className="rating__container">
+                      <button><UpTriangle/></button>
+                      <span className="rating__int">+1</span>
+                      <button><DownTriangle/></button>
+                  </span>
+              </div>
+          </div>
+          <div className="chat__col right">
+            <div className="messageBody">
+                <span className="username">Frodo Baggins</span>
+                <span className="message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et esse iusto maiores sit omnis itaque minus sint. Ullam, quis.</span>
+            </div>
+            <div className="messageMeta">
+                September 20, 2021&nbsp;•&nbsp;
+                <button
+                  style={{color: "#E52646"}}
+                >
+                    Delete
+                </button>
+            </div>
+          </div>
         </div>
       </li>
     )
