609d83f8a5b7d3252e13638a91a100e472d17c8e
PatrickFortaleza <patrickfortaleza@gmail.com>
2020-12-09 10:31:19 -0800

    map login fields to state

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,36 +3,52 @@ import { Link } from 'react-router-dom';
 import { MailIcon, LockIcon, PictureIcon } from '../Icons'
 
 export class Login extends Component {
-    render() {
-        return (
-        <section className="Login">
-          <div className="form__container">
-            <div className="modal-form">
-                <div className="modal-head">
-                    <h3 className="modal-title">Login</h3>
-                </div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="fieldset">
-                      <MailIcon />
-                      <input type="text" id="email" name="email" placeholder="email"/>
-                    </div>
-
-                    <div className="fieldset">
-                      <LockIcon />
-                      <input type="password" id="password" placeholder="password" name="password"/>
-                    </div>
-                    <div className="fieldset submit">
-                        <button className="submit">Login</button>
-                    </div>
-                </form>
-            </div>
-            <div className="modal-footer">
-                <p>Don't have an account? <Link to="/register">Sign up</Link></p>
-            </div>
-          </div>
-        </section>
-        )
+  constructor(props){
+    super(props)
+    this.state = {
+      email: "",
+      password: "",
     }
+  }
+
+  onInputChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <section className="Login">
+        <div className="form__container">
+          <div className="modal-form">
+              <div className="modal-head">
+                  <h3 className="modal-title">Login</h3>
+              </div>
+              <form onSubmit={this.onSubmit}>
+                  <div className="fieldset">
+                    <MailIcon />
+                    <input type="text" id="email" name="email" placeholder="email" value={ email } onChange={this.onInputChange}/>
+                  </div>
+
+                  <div className="fieldset">
+                    <LockIcon />
+                    <input type="password" id="password" placeholder="password" name="password"
+                    value={ password } onChange={this.onInputChange}/>
+                  </div>
+                  <div className="fieldset submit">
+                      <button className="submit">Login</button>
+                  </div>
+              </form>
+          </div>
+          <div className="modal-footer">
+              <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default Login
