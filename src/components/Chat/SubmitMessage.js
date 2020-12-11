895300d9787047d9ba404768039395cda2e07bd0
apurugganan <apurugganan1@my.bcit.ca>
2020-12-10 16:17:13 -0800

    onpost chat | Ok | goes to database

--- src/components/Chat/SubmitMessage.js
+++ src/components/Chat/SubmitMessage.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, {Component } from 'react'
+const BASE_URL = 'https://localhost:44363/api/';
 
 export class SubmitMessage extends Component {
   constructor(props){
@@ -15,6 +16,44 @@ export class SubmitMessage extends Component {
     });
   }
 
+
+  createMessage = async event => {
+      event.preventDefault(); 
+
+      // fetch
+      fetch(BASE_URL+'Chat', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            UserName: this.state.username,  
+            MessageText: this.state.message
+        })
+    })
+    // Response received.
+    .then(response => response.json())
+        // Data retrieved.
+        .then(json => {
+            console.log(JSON.stringify(json));
+            this.setState({message:""});
+           // Clear input. 
+            //fetchMessages();
+        })
+        // Data not retrieved.
+        .catch(function (error) {
+            console.log(error);
+        })
+  }
+
+  onInputChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  
   render() {
       return (
         <section className="SubmitMessage">
@@ -32,7 +71,7 @@ export class SubmitMessage extends Component {
               </label>
               <div className="fieldset">
                 <input type="text" placeholder="Aa" id="message" value={this.state.message} onChange={(e) => this.onInputChange(e)}/>
-                <button>Send</button>
+                <button className="button" onClick={this.createMessage}>Send</button>
               </div>
             </form>
           </div>
