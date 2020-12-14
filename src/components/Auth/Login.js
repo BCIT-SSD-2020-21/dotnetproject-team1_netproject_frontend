8cfb7c72811af9b593207a5eb17ba49fe4b1b609
PatrickFortaleza <patrickfortaleza@gmail.com>
2020-12-14 08:15:27 -0800

    resolve conflitcs in login.js

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { MailIcon, LockIcon } from '../Icons'
 
+
 class Login extends Component {
   state = {
     email: "",
