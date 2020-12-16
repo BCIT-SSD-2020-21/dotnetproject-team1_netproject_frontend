a9d44b4ffbca8ded514abf239233b1a4e365a794
Kalvin Tang <ktang82@my.bcit.ca>
2020-12-16 11:22:37 -0800

    Update validation for register #33

--- src/components/Auth/Registration.js
+++ src/components/Auth/Registration.js
@@ -7,8 +7,11 @@ class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: {},
-            errors: {},
+            email: "",
+            password: "",
+            isValid: true,
+            confirmpassword: "",
+            errors: [],
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -21,71 +24,101 @@ class Register extends Component {
         this.setState({ input });
     }
 
-    validate() {
-        let input = this.state.input;
-        let errors = {};
-        let isValid = true;
+    validate(event) {
+        event.preventDefault();
+        console.log(this.state.email);
+        let email = this.state.email;
+        let password = this.state.password;
+        let confirmpassword = this.state.password;
 
-        //email validation
-        if (!input["email"]) {
-            isValid = false;
-            errors["email"] = "Please enter your email address";
+        if (!email) {
+            this.setState({ isValid: false });
+            let errors = this.state.errors;
+            let errorMessage = "Please enter your email address";
+            errors.push(errorMessage);
         }
 
-        if (typeof input["email"] !== "undefined") {
-            var pattern = new RegExp(
-                /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
-            );
-            if (!pattern.test(input["email"])) {
-                isValid = false;
-                errors["email"] = "Please enter a valid email address";
-            }
-
-            //password and paswword match validation
-        }
-        if (!input["password"]) {
-            isValid = false;
-            errors["password"] = "Please enter your password.";
-        }
-        if (!input["confirmpassword"]) {
-            isValid = false;
-            errors["password"] = "Please enter the confirmation password.";
+        let pattern = new RegExp(
+            /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+        );
+        if (!pattern.test(email)) {
+            this.setState({ isValid: false });
+            let errors = this.state.errors;
+            let errorMessage = "Not a valid email address";
+            errors.push(errorMessage);
         }
 
-        if (
-            typeof input["password"] !== "undefined" &&
-            typeof input["confirmpassword"] !== "undefined"
-        ) {
-            if (input["password"] != input["confirmpassword"]) {
-                isValid = false;
-                errors["password"] = "Password do not match";
-            }
-            //password requirements
-            if (
-                !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,15}$/.test(
-                    input["password"]
-                )
-            ) {
-                isValid = false;
-                errors["password"] =
-                    "Password must be minimum 7 and max 10 character";
-                //(?=.*\d)          should contain at least one digit
-                //(?=.*[a-z])       should contain at least one lower case
-                //(?=.*[A-Z])       should contain at least one upper case
-                //[0-9a-zA-Z]{7,10} should contain at least 7 to 15 characters
-            }
-        }
+        let passPattern = new RegExp(
+            /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/
+        );
 
-        //this.setState({errors:errors});
-        // return isValid;
+        if (!passPattern.test(password)) {
+            this.setState({ isValid: false });
+            let errors = this.state.errors;
+            let passMessage =
+                "Please ensure the password has at least Upper case, a lower case, a number, and a Min 7 characters.";
+            errors.push(passMessage);
+        }
     }
 
-    //state variables for form inputs and errors
-    state = {
-        email: "",
-        password: "",
-        confirmpassword: "",
-    };
+    // validate() {
+    //     let input = this.state.input;
+    //     let errors = {};
+    //     let isValid = true;
+
+    //     //email validation
+    //     if (!input["email"]) {
+    //         isValid = false;
+    //         errors["email"] = "Please enter your email address";
+    //     }
+
+    //     if (typeof input["email"] !== "undefined") {
+    //         var pattern = new RegExp(
+    //             /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    //         );
+    //         if (!pattern.test(input["email"])) {
+    //             isValid = false;
+    //             errors["email"] = "Please enter a valid email address";
+    //         }
+
+    //         //password and paswword match validation
+    //     }
+    //     if (!input["password"]) {
+    //         isValid = false;
+    //         errors["password"] = "Please enter your password.";
+    //     }
+    //     if (!input["confirmpassword"]) {
+    //         isValid = false;
+    //         errors["password"] = "Please enter the confirmation password.";
+    //     }
+
+    //     if (
+    //         typeof input["password"] !== "undefined" &&
+    //         typeof input["confirmpassword"] !== "undefined"
+    //     ) {
+    //         if (input["password"] != input["confirmpassword"]) {
+    //             isValid = false;
+    //             errors["password"] = "Password do not match";
+    //         }
+    //         //password requirements
+    //         if (
+    //             !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,15}$/.test(
+    //                 input["password"]
+    //             )
+    //         ) {
+    //             isValid = false;
+    //             errors["password"] =
+    //                 "Password must be minimum 7 and max 10 character";
+    //             //(?=.*\d)          should contain at least one digit
+    //             //(?=.*[a-z])       should contain at least one lower case
+    //             //(?=.*[A-Z])       should contain at least one upper case
+    //             //[0-9a-zA-Z]{7,10} should contain at least 7 to 15 characters
+    //         }
+    //     }
+
+    //     //this.setState({errors:errors});
+    //     // return isValid;
+    // }
 
     handleSubmit = async (event) => {
         //Prevent page reload
@@ -130,13 +163,20 @@ class Register extends Component {
     };
 
     render() {
+        const { isValid, errors } = this.state;
         return (
             <section className="Login">
                 <div className="form__container">
                     <div className="modal-form">
                         <div className="modal-head">
                             <h3 className="modal-title">Register</h3>
-                            <form onSubmit={this.handleSubmit}>
+
+                            {isValid
+                                ? null
+                                : errors.map((err) => {
+                                      return <div>{err}</div>;
+                                  })}
+                            <form onSubmit={(e) => this.validate(e)}>
                                 <div className="fieldset">
                                     <MailIcon />
                                     <input
@@ -147,10 +187,6 @@ class Register extends Component {
                                         value={this.state.email}
                                         onChange={this.onInputChange}
                                     />
-                                </div>
-
-                                <div style={{ fontSize: 16, color: "red" }}>
-                                    {this.state.errors.email}
                                 </div>
 
                                 <div className="fieldset">
