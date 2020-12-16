9bda9bcc6cb803c623e5fbb3c091e403abfd9e89
Kalvin Tang <ktang82@my.bcit.ca>
2020-12-16 15:37:53 -0800

    update error handling message #33

--- src/components/Auth/Registration.js
+++ src/components/Auth/Registration.js
@@ -24,12 +24,16 @@ class Register extends Component {
         this.setState({ input });
     }
 
+    resetErrors() {
+        this.setState({ errors: [] });
+    }
+
     validate(event) {
         event.preventDefault();
         console.log(this.state.email);
         let email = this.state.email;
         let password = this.state.password;
-        let confirmpassword = this.state.password;
+        let confirmpassword = this.state.confirmpassword;
 
         if (!email) {
             this.setState({ isValid: false });
@@ -45,6 +49,7 @@ class Register extends Component {
             this.setState({ isValid: false });
             let errors = this.state.errors;
             let errorMessage = "Not a valid email address";
+
             errors.push(errorMessage);
         }
 
@@ -54,19 +59,20 @@ class Register extends Component {
         if (!upperCase.test(password)) {
             this.setState({ isValid: false });
             let errors = this.state.errors;
-            let passMessage =
+            let errorMessage =
                 "Please ensure the password includes 1 uppercase letter";
-            errors.push(passMessage);
+            errors.push(errorMessage);
         }
+
         let lowerCase = new RegExp(/^(?=.*[a-z])/);
 
         //checks for lowercase
         if (!lowerCase.test(password)) {
             this.setState({ isValid: false });
             let errors = this.state.errors;
-            let passMessage =
+            let errorMessage =
                 "Please ensure the password includes 1 lowercase letter";
-            errors.push(passMessage);
+            errors.push(errorMessage);
         }
 
         let digits = new RegExp(/^(?=.*[0-9])/);
@@ -74,8 +80,8 @@ class Register extends Component {
         if (!digits.test(password)) {
             this.setState({ isValid: false });
             let errors = this.state.errors;
-            let passMessage = "Please ensure the password includes a number";
-            errors.push(passMessage);
+            let errorMessage = "Please ensure the password includes a number";
+            errors.push(errorMessage);
         }
 
         let special = new RegExp(/^(?=.*[!@#$&*])/);
@@ -84,70 +90,29 @@ class Register extends Component {
         if (!special.test(password)) {
             this.setState({ isValid: false });
             let errors = this.state.errors;
-            let passMessage =
+            let errorMessage =
                 "Please ensure the password includes a special !, @, #, $, or &* character";
-            errors.push(passMessage);
+            errors.push(errorMessage);
+        }
+
+        let passLength = new RegExp(/^(?=.*[A-Za-z\d$@$!%*?&]{7})/);
+
+        //Checks the password length
+        if (!passLength.test(password)) {
+            this.setState({ isValid: false });
+            let errors = this.state.errors;
+            let errorMessage = "Password length must be a min of 7";
+            errors.push(errorMessage);
+        }
+
+        //Check matching password
+        if (confirmpassword != password) {
+            this.setState({ isValid: false });
+            let errors = this.state.errors;
+            let errorMessage = "The passwords do not match";
+            errors.push(errorMessage);
         }
     }
-    //   /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/
-    // validate() {
-    //     let input = this.state.input;
-    //     let errors = {};
-    //     let isValid = true;
-
-    //     //email validation
-    //     if (!input["email"]) {
-    //         isValid = false;
-    //         errors["email"] = "Please enter your email address";
-    //     }
-
-    //     if (typeof input["email"] !== "undefined") {
-    //         var pattern = new RegExp(
-    //             /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
-    //         );
-    //         if (!pattern.test(input["email"])) {
-    //             isValid = false;
-    //             errors["email"] = "Please enter a valid email address";
-    //         }
-
-    //         //password and paswword match validation
-    //     }
-    //     if (!input["password"]) {
-    //         isValid = false;
-    //         errors["password"] = "Please enter your password.";
-    //     }
-    //     if (!input["confirmpassword"]) {
-    //         isValid = false;
-    //         errors["password"] = "Please enter the confirmation password.";
-    //     }
-
-    //     if (
-    //         typeof input["password"] !== "undefined" &&
-    //         typeof input["confirmpassword"] !== "undefined"
-    //     ) {
-    //         if (input["password"] != input["confirmpassword"]) {
-    //             isValid = false;
-    //             errors["password"] = "Password do not match";
-    //         }
-    //         //password requirements
-    //         if (
-    //             !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,15}$/.test(
-    //                 input["password"]
-    //             )
-    //         ) {
-    //             isValid = false;
-    //             errors["password"] =
-    //                 "Password must be minimum 7 and max 10 character";
-    //             //(?=.*\d)          should contain at least one digit
-    //             //(?=.*[a-z])       should contain at least one lower case
-    //             //(?=.*[A-Z])       should contain at least one upper case
-    //             //[0-9a-zA-Z]{7,10} should contain at least 7 to 15 characters
-    //         }
-    //     }
-
-    //     //this.setState({errors:errors});
-    //     // return isValid;
-    // }
 
     handleSubmit = async (event) => {
         //Prevent page reload
@@ -205,7 +170,10 @@ class Register extends Component {
                                 : errors.map((err) => {
                                       return <div>{err}</div>;
                                   })}
-                            <form onSubmit={(e) => this.validate(e)}>
+                            <form
+                                onSubmit={(e) => this.validate(e)}
+                                onChange={(e) => this.resetErrors(e)}
+                            >
                                 <div className="fieldset">
                                     <MailIcon />
                                     <input
@@ -264,4 +232,5 @@ class Register extends Component {
         );
     }
 }
+
 export default withRouter(Register);
