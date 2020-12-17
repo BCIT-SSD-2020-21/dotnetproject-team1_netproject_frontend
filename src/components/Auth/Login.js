695a7c7a370ad84c907a04e2bc57845e7ee605ab
JuHwan Moon <juhwan1014@naver.com>
2020-12-18 03:31:58 +0900

    refactoring

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -17,43 +17,43 @@ class Login extends Component {
   handleSubmit = async event => {
     //Prevent page reload
     event.preventDefault();
-    
+
     //Integrate Auth here on valid form submission
-fetch('https://parlezprod.azurewebsites.net/Auth/Login', {
-  method: 'POST',
-  headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-      Email: this.state.email,
-      Password: this.state.password
-  })
-})
-// Response received.
-.then(response => response.json())
-// Data retrieved.
-.then(json => {
-  console.log(JSON.stringify(json));
-  // Store token with session data.
-  if(json["status"]==="OK") {
-      sessionStorage.setItem('bearer-token', json["token"]);
-      sessionStorage.setItem('authUserName', json["username"])
-      sessionStorage.setItem('userId', json["userid"])
-      this.props.history.push("/")
-  }
-  else {
-      // error message handling
-      this.setState({error: {message: 'The validation is failed. Please check again your email and password.', active: true,} })
-      console.log('Error in Auth/Login');
-     
-  }
-})
-// Data not retrieved.
-.catch(function (error) {
-  console.log(error);
-})
-    
+    fetch('https://parlezprod.azurewebsites.net/Auth/Login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        Email: this.state.email,
+        Password: this.state.password
+      })
+    })
+      // Response received.
+      .then(response => response.json())
+      // Data retrieved.
+      .then(json => {
+        console.log(JSON.stringify(json));
+        // Store token with session data.
+        if (json["status"] === "OK") {
+          sessionStorage.setItem('bearer-token', json["token"]);
+          sessionStorage.setItem('authUserName', json["username"])
+          sessionStorage.setItem('userId', json["userid"])
+          this.props.history.push("/")
+        }
+        else {
+          // error message handling
+          this.setState({ error: { message: 'The validation is failed. Please check again your email and password.', active: true, } })
+          console.log('Error in Auth/Login');
+
+        }
+      })
+      // Data not retrieved.
+      .catch(function (error) {
+        console.log(error);
+      })
+
   };
 
 
@@ -61,41 +61,41 @@ fetch('https://parlezprod.azurewebsites.net/Auth/Login', {
     e.preventDefault();
     const { email, password } = this.state;
 
- 
-
-    let isEmail = new RegExp( /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    let isEmail = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     let passLength = new RegExp(/^(?=.*[A-Za-z\d$@$!%*?&]{7})/);
     let upperCase = new RegExp(/^(?=.*[A-Z])/);
     let lowerCase = new RegExp(/^(?=.*[a-z])/);
     let digits = new RegExp(/^(?=.*[0-9])/);
     let special = new RegExp(/^(?=.*[!@#$&*])/);
-    
 
-
-    if(!email && !password){
-      this.setState({error: {message: 'You have to enter both your email and password.', active: true,} })
+    if (!email && !password) {
+      this.setState({ error: { message: 'You have to enter both your email and password.', active: true, } })
       return null
-    }else if(!email){
-      this.setState({error: {message: 'Please enter your email.', active: true,} })
+    } else if (!email) {
+      this.setState({ error: { message: 'Please enter your email.', active: true, } })
       return null
-    }else if(!password){
-      this.setState({error: {message: 'Please enter your password.', active: true,} })
+    } else if (!password) {
+      this.setState({ error: { message: 'Please enter your password.', active: true, } })
       return null
-    }else if(!isEmail.test(email)){
-      this.setState({error: {message: 'Your Email is not valid. Please check your Email again.', active: true,} })
-    }else if(!passLength.test(password)){
-      this.setState({error: {message: 'Password must be at least 7 characters', active: true,} })
-    }else if(!upperCase.test(password)){
-      this.setState({error: {message: 'Password needs to include an uppercase letter', active: true,} })
-    }else if(!lowerCase.test(password)){
-      this.setState({error: {message: 'Password needs to include a lowercase letter', active: true,} })
-    }else if(!digits.test(password)){
-      this.setState({error: {message: 'Password needs to include a number', active: true,} })
-    }else if(!special.test(password)){
-      this.setState({error: {message: 'asswords needs a special character !, @, #, $, or &', active: true,} })
+    } else if (!isEmail.test(email)) {
+      this.setState({ error: { message: 'Your Email is not valid. Please check your Email again.', active: true, } })
+    } else if (!passLength.test(password)) 
+    {
+      this.setState({ error: { message: 'Password must be at least 7 characters', active: true, } })
+    } else if (!upperCase.test(password)) 
+    {
+      this.setState({ error: { message: 'Password needs to include an uppercase letter', active: true, } })
+    } else if (!lowerCase.test(password)) 
+    {
+      this.setState({ error: { message: 'Password needs to include a lowercase letter', active: true, } })
+    } else if (!digits.test(password))
+     {
+      this.setState({ error: { message: 'Password needs to include a number', active: true, } })
+    } else if (!special.test(password)) 
+    {
+      this.setState({ error: { message: 'Password needs a special character !, @, #, $, or &', active: true, } })
     }
-    
-    else{
+    else {
       this.handleSubmit(e)
     }
   }
@@ -104,10 +104,12 @@ fetch('https://parlezprod.azurewebsites.net/Auth/Login', {
 
   resetErrors = () => {
     setTimeout(() => {
-      this.setState({ error: {
-        message: '',
-        active: false
-      }}) 
+      this.setState({
+        error: {
+          message: '',
+          active: false
+        }
+      })
     }, 4000)
   }
 
@@ -120,8 +122,7 @@ fetch('https://parlezprod.azurewebsites.net/Auth/Login', {
   };
 
   render() {
-    const {  error } = this.state;
-
+    const { error } = this.state;
 
     const errorMessage = (
       <Fragment>
@@ -130,54 +131,53 @@ fetch('https://parlezprod.azurewebsites.net/Auth/Login', {
             <div className="svg-cont">
               <CautionIcon />
             </div>
-            <p>{ error.message }</p>
+            <p>{error.message}</p>
           </div>
         </div>
       </Fragment>
     )
 
-
-
     return (
       <section className="Login">
-         <div className="form__container">
-            <div className="modal-form">
-              <div className="modal-head">
-                    <h3 className="modal-title">Login</h3>
-              </div>
-          <form onSubmit= {(e) => this.validateLogin(e)}>
-            <div className="fieldset">
+        <div className="form__container">
+          <div className="modal-form">
+            <div className="modal-head">
+              <h3 className="modal-title">Login</h3>
+            </div>
+            <form onSubmit={(e) => this.validateLogin(e)}>
+              <div className="fieldset">
                 <MailIcon />
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="text"
                   id="email"
                   placeholder="Enter email"
                   value={this.state.email}
                   onChange={this.onInputChange}
                 />
-            </div>
+              </div>
 
-            <div className="fieldset">
+              <div className="fieldset">
                 <LockIcon />
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="password"
                   id="password"
                   placeholder="Password"
                   value={this.state.password}
                   onChange={this.onInputChange}
                 />
-            </div>
+              </div>
 
-            <div className="fieldset submit">
-                        <button className="submit">Login</button>
-                    </div>
-          </form>
+              <div className="fieldset submit">
+
+                <button className="submit">Login</button>
+              </div>
+            </form>
           </div>
           <div className="modal-footer">
-                <p>Don't have an account? <Link to="/register">Sign up</Link></p>
-            </div>
+            <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+          </div>
         </div>
         { error.active ? errorMessage : ''}
       </section>
